04ef49d382aa94f2bc1af8b1e7ca91efa4f613de
jackhowa <johncarverhoward@gmail.com>
2017-07-02 21:48:23 -0500

    getting error with url generated record

--- PitchPerfect/PlaySoundsViewController.swift
+++ PitchPerfect/PlaySoundsViewController.swift
@@ -7,9 +7,9 @@
 //
 
 import UIKit
+import AVFoundation
 
 class PlaySoundsViewController: UIViewController {
-    var recordedAudioURL: URL!
     
     @IBOutlet weak var slowButton: UIButton!
 
@@ -19,17 +19,48 @@ class PlaySoundsViewController: UIViewController {
     
     @IBOutlet weak var lowPitchButton: UIButton!
     
+    @IBOutlet weak var echoButton: UIButton!
+    
+    @IBOutlet weak var reverbButton: UIButton!
+    
+    @IBOutlet weak var pauseButton: UIButton!
+    
+    var recordedAudioURL:URL!
+    var audioFile:AVAudioFile!
+    var audioEngine:AVAudioEngine!
+    var audioPlayerNode: AVAudioPlayerNode!
+    var stopTimer: Timer!
+    
+    enum ButtonType: Int {
+        case slow = 0, fast, chipmunk, vader, echo, reverb
+    }
+    
+    
+    @IBAction func playSoundForButton(_ sender: UIButton) {
+        print("Play Sound Button Pressed")
+    }
+    
+    @IBAction func stopButtonPressed(_ sender: AnyObject) {
+        print("Stop Audio Button Pressed")
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        setupAudio()
 
         // Do any additional setup after loading the view.
     }
+    
+    
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
     
+    override func viewWillAppear(_ animated: Bool) {
+        configureUI(.notPlaying)
+    }
 
     /*
     // MARK: - Navigation
